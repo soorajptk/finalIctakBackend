917f3afe4c684a7d5b9c9aaac6e54e935328ed61
soorajptk <soorajptk1997@gmail.com>
2022-03-11 21:58:12 -0800

    dd

--- utils/nodemailerConfig.js
+++ utils/nodemailerConfig.js
@@ -1,10 +1,8 @@
 
 module.exports={
-           host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'demarcus.king1@ethereal.email',
-        pass: 'naF7n4ss5PWZNmyMUU'
-
-    }
+       service:'gmail',
+        auth:{
+            user:'soorajsurendran8196@gmail.com',
+            pass:'Soorajptk@upaobca011'
+        }      
     }
